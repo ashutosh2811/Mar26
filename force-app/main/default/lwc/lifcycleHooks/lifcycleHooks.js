b2b2e138f5dadc84d1197d4d1c62ebcacac473f1
Ashutosh Sinha <ashutosh.sinha@kantata.com>
2026-04-04 15:26:47 +0530

    more excercises

--- force-app/main/default/lwc/lifcycleHooks/lifcycleHooks.js
+++ force-app/main/default/lwc/lifcycleHooks/lifcycleHooks.js
@@ -3,18 +3,18 @@ import { LightningElement } from 'lwc';
 export default class LifcycleHooks extends LightningElement {
     constructor() {
         super();
-        console.log('Constructor: Component is being constructed');
+        console.log('LifcycleHooks Constructor: Component is being constructed');
     }
 
     connectedCallback() {
-        console.log('Connected Callback: Component is inserted into the DOM');
+        console.log('LifcycleHooks Connected Callback: Component is inserted into the DOM');
     }
 
     renderedCallback() {
-        console.log('Rendered Callback: Component has been rendered');
+        console.log('LifcycleHooks Rendered Callback: Component has been rendered');
     }
 
     disconnectedCallback() {
-        console.log('Disconnected Callback: Component is removed from the DOM');
+        console.log('LifcycleHooks Disconnected Callback: Component is removed from the DOM');
     }  
 }
